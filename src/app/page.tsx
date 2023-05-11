969012244dc9abe8f943643a5d06210be35b2a86
ortizvictorw <57049891+ortizvictorw@users.noreply.github.com>
2023-05-11 20:38:39 -0300

    cambio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function HomePage () {
   return (
     <section>
-      <Link href='/sound'>Hola Mundo</Link>
+      <Link href='/sound'>Hola Elias</Link>
     </section>
   )
 }
